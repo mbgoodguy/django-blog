2b0b388eaa4e90661a8e2d6e0d10a3f10e1ed8ea
mb <nerooren4001@gmail.com>
2023-11-14 21:58:16 +0300

    Add form and comments into post_detail context for using in template

--- django4_blog/blog/views.py
+++ django4_blog/blog/views.py
@@ -1,11 +1,12 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from .forms import EmailPostForm
 # Create your views here.
-from .models import Post
+from .models import Post, CommentForm
 
 
 class PostListView(ListView):
@@ -24,8 +25,10 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         slug=post,
     )
+    form = CommentForm()
+    comments = post.comments.filter(active=True)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -47,3 +50,14 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})

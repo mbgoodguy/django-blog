2bd11d2591c0127f2efea42b933b4b84b97d0ada
mb <nerooren4001@gmail.com>
2023-11-15 05:24:21 +0300

    freeze requirements, add ipython and django_extensions

--- django4_blog/django4_blog/settings.py
+++ django4_blog/django4_blog/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'shell_plus',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
